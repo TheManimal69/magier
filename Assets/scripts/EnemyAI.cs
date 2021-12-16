acd2630b33f6478a56acb2c48acca581a1b27380
freakyjoestarman <boarmanmitch@gmail.com>
2021-12-16 16:38:37 -0600

    added player combat and eneny damage

--- Assets/scripts/EnemyAI.cs
+++ Assets/scripts/EnemyAI.cs
@@ -11,6 +11,8 @@ public class EnemyAI : MonoBehaviour
     public float speed = 100f;
     public Transform enemyGFX;
 
+    [SerializeField] float maxHealth = 3;
+    float currentHealth;
 
     Path path;
 
@@ -19,13 +21,16 @@ public class EnemyAI : MonoBehaviour
     //References
     Seeker seeker;
     Rigidbody2D rb;
+
+
     // Start is called before the first frame update
     void Start(   )
     {
         seeker = GetComponent<Seeker>();
         rb = GetComponent<Rigidbody2D>();
         InvokeRepeating("UpdatePath", 0f, 0.5f);
-            //to do create update path method
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -79,4 +84,19 @@ public class EnemyAI : MonoBehaviour
             currentWaypoint = 0;
         }
     }
-}
+
+    public void ProcessHit(float damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+
+        }
+
+
+
+    }
+
+}   
